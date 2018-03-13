d24943766dc266e9715e0c7bc586a1c5cc75d147
Dmitrii <Simasorokin@gmail.com>
2018-03-13 13:17:11 +0300

    Добавил ввод с клавиатуры, и вызов функции сложения со значениями введенными с клавиатуры.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,17 +11,19 @@ public class Main {
         int input = in.nextInt();
         System.out.println("Your input is: " + input);
 
+        System.out.println("Enter some  number: ");
+        int input2 = in.nextInt();
+        System.out.println("Your input is: " + input2);
 
-        int c = Sum(6,7);
-        System.out.println(c);
+
+        int y = Sum(input,input2);
+        System.out.println("The answer is = " + y);
 
     }
 
 
-    public static int Sum (int a, int b){
+    public static int Sum(int a, int b){
         int c = a+b;
-
-
         return c;
     }
 
